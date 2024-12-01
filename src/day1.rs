ce671767ed5814e94d8051149a0fc91ad9278299
L4Vo5 <l4vo5finalboss@gmail.com>
2024-12-01 19:00:41 -0300

    optimized part 2 parse

--- src/day1.rs
+++ src/day1.rs
@@ -58,9 +58,49 @@ pub fn part1_original(whole: &str) -> i64 {
     sum as i64
 }
 
+
+pub fn part2(whole: &str) -> i64 {
+    // Take advantage of the fact that the format for each line is:
+    // (S = Space)
+    const FORMAT: &str = "AAAAASSSBBBBB\n";
+    const ZERO: u8 = b'0';
+    let whole = whole.as_bytes();
+    let line_count = whole.len() / FORMAT.len();
+    let mut nums_1 = vec![0; line_count];
+    let mut nums_2 = vec![0; line_count];
+    for (i, chunk) in whole.chunks_exact(FORMAT.len()).enumerate() {
+        let n1 = 
+        (chunk[4] - ZERO) as u32 +
+        (chunk[3] - ZERO) as u32 * 10 +
+        (chunk[2] - ZERO) as u32 * 100 +
+        (chunk[1] - ZERO) as u32 * 1000 +
+        (chunk[0] - ZERO) as u32 * 10000;
+        let n2 = 
+        (chunk[12] - ZERO) as u32 +
+        (chunk[11] - ZERO) as u32 * 10 +
+        (chunk[10] - ZERO) as u32 * 100 +
+        (chunk[9] - ZERO) as u32 * 1000 +
+        (chunk[8] - ZERO) as u32 * 10000;
+        nums_1[i] = n1;
+        nums_2[i] = n2;
+    }
+    let mut sum = 0;
+    for i in 0..nums_1.len() {
+        let n = nums_1[i];
+        let mut a = 0;
+        for j in 0..nums_2.len() {
+            if nums_2[j] == n {
+                a += 1;
+            }
+        }
+        sum += n * a;
+    }
+    sum as i64
+}
+
 /// AoC placement:
 /// Time 00:04:52 rank 710
-pub fn part2(whole: &str) -> i64 {
+pub fn part2_original(whole: &str) -> i64 {
 
     let lines = get_lines(&whole);
     let nums = get_numbers(&whole);
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod day1;
 
 fn main() {
     let day = 1;
-    let part = 1;
+    let part = 2;
 
     let contents = read_file(&day.to_string());
     let start = Instant::now();
