43a2458e37cd329215e3d792982f1654242c8eae
L4Vo5 <l4vo5finalboss@gmail.com>
2024-12-01 03:58:40 -0300

    added library section to check out the codspeed challenge (it also helps organize my solutions lol)

---
+++ src/day1.rs
@@ -0,0 +1,56 @@
+use crate::utils::*;
+
+/// AoC placement:
+/// Time 00:03:10 rank 857
+pub fn part_1(whole: &str) -> String {
+    let lines = get_lines(&whole);
+    let nums = get_numbers(&whole);
+    let mut nums_1 = vec![];
+    let mut nums_2 = vec![];
+    for i in 0..nums.len() {
+        if i % 2 == 0 {
+            nums_1.push(nums[i].num);
+        } else {
+            nums_2.push(nums[i].num);
+        }
+    }
+    nums_1.sort();
+    nums_2.sort();
+    let mut sum = 0;
+    for i in 0..nums_1.len() {
+
+        sum += (nums_1[i] as i64 - nums_2[i] as i64).abs();
+    }
+    sum.to_string()
+}
+
+/// AoC placement:
+/// Time 00:04:52 rank 710
+pub fn part_2(whole: &str) -> String {
+
+    let lines = get_lines(&whole);
+    let nums = get_numbers(&whole);
+    let mut nums_1 = vec![];
+    let mut nums_2 = vec![];
+    for i in 0..nums.len() {
+        if i % 2 == 0 {
+            nums_1.push(nums[i].num);
+        } else {
+            nums_2.push(nums[i].num);
+        }
+    }
+    nums_1.sort();
+    nums_2.sort();
+    let mut sum = 0;
+    for i in 0..nums_1.len() {
+        let n = nums_1[i];
+        let mut a = 0;
+        for j in 0..nums_2.len() {
+            if nums_2[j] == n {
+                a += 1;
+            }
+        }
+        sum += n * a;
+    }
+    sum.to_string()
+}
---
+++ src/utils.rs
@@ -0,0 +1,60 @@
+use std::fs;
+
+
+pub fn read_file(filename: &str) -> String {
+    let path = format!("inputs/{}.txt", filename);
+    fs::read_to_string(path).unwrap()
+}
+
+pub fn get_lines(content: &str) -> Vec<String> {
+    content.split("\n").map(|s| s.to_string()).collect()
+}
+
+pub fn split_collect<'a>(s: &'a str, c: &str) -> Vec<&'a str> {
+    s.split(c).collect()
+}
+
+pub fn get_numbers(s: &str) -> Vec<NumberPos> {
+    let mut v = vec![];
+    let mut last_was_num = false;
+    let mut i = 0;
+    let mut real_i = 0;
+    for (y, line) in s.lines().enumerate() {
+        for (x, char) in line.chars().enumerate() {
+            if char.is_numeric() {
+                let this_num = char.to_digit(10).unwrap() as u64;
+                if last_was_num {
+                    let last: &mut NumberPos = v.last_mut().unwrap();
+                    last.num = last.num * 10 + this_num;
+                    last.s_num = last.num as i64 * last.sign;
+                    last.length += 1;
+                } else {
+                    let mut sign = 1;
+                    if i != 0 && &s[i-1..=i-1] == "-" {sign = -1;}
+                    let s_num = this_num as i64 * sign;
+                    v.push(NumberPos { num: this_num, s_num, i, x, y, real_i, length: if sign == 1 {1} else {2}, sign})
+                }
+                last_was_num = true;
+            } else {
+                last_was_num = false;
+            }
+            i += 1;
+            real_i += 1;
+        }
+        last_was_num = false;
+        real_i += 1;
+    }
+    v
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct NumberPos {
+    pub num: u64,
+    pub s_num: i64,
+    pub sign: i64, // kind of a detail of how parsing is implemented oops.
+    pub i: usize, // i of the first NUMBER. if there's a minus sign before, that's in position i-1. same applies to real_i and x.
+    pub real_i: usize, // taking \n into account
+    pub length: usize, // length of the number in characters (-0004 is length 5)
+    pub x: usize,
+    pub y: usize,
+}
