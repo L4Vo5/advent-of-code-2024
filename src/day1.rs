5c55db1ff544d0cc33fd4896b2308eba3894a8fc
L4Vo5 <l4vo5finalboss@gmail.com>
2024-12-01 18:56:45 -0300

    optimized day 1 part 1 as best as I could

--- src/day1.rs
+++ src/day1.rs
@@ -1,8 +1,42 @@
 use crate::utils::*;
 
+pub fn part1(whole: &str) -> i64 {
+    // Take advantage of the fact that the format for each line is:
+    // (S = Space)
+    const FORMAT: &str = "AAAAASSSBBBBB\n";
+    const ZERO: u8 = b'0';
+    let whole = whole.as_bytes();
+    let line_count = whole.len() / FORMAT.len();
+    let mut nums_1 = vec![0; line_count];
+    let mut nums_2 = vec![0; line_count];
+    for (i, chunk) in whole.chunks_exact(FORMAT.len()).enumerate() {
+        let n1 = 
+        (chunk[4] - ZERO) as u32 +
+        (chunk[3] - ZERO) as u32 * 10 +
+        (chunk[2] - ZERO) as u32 * 100 +
+        (chunk[1] - ZERO) as u32 * 1000 +
+        (chunk[0] - ZERO) as u32 * 10000;
+        let n2 = 
+        (chunk[12] - ZERO) as u32 +
+        (chunk[11] - ZERO) as u32 * 10 +
+        (chunk[10] - ZERO) as u32 * 100 +
+        (chunk[9] - ZERO) as u32 * 1000 +
+        (chunk[8] - ZERO) as u32 * 10000;
+        nums_1[i] = n1;
+        nums_2[i] = n2;
+    }
+    nums_1.sort();
+    nums_2.sort();
+    let mut sum = 0;
+    for i in 0..line_count {
+        sum += nums_1[i].abs_diff(nums_2[i]);
+    }
+    sum as i64
+}
+
 /// AoC placement:
 /// Time 00:03:10 rank 857
-pub fn part1(whole: &str) -> String {
+pub fn part1_original(whole: &str) -> i64 {
     let lines = get_lines(&whole);
     let nums = get_numbers(&whole);
     let mut nums_1 = vec![];
@@ -21,12 +55,12 @@ pub fn part1(whole: &str) -> String {
 
         sum += (nums_1[i] as i64 - nums_2[i] as i64).abs();
     }
-    sum.to_string()
+    sum as i64
 }
 
 /// AoC placement:
 /// Time 00:04:52 rank 710
-pub fn part2(whole: &str) -> String {
+pub fn part2(whole: &str) -> i64 {
 
     let lines = get_lines(&whole);
     let nums = get_numbers(&whole);
@@ -52,5 +86,5 @@ pub fn part2(whole: &str) -> String {
         }
         sum += n * a;
     }
-    sum.to_string()
+    sum as i64
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use core::num;
 use std::{fs, collections::{HashSet, HashMap, VecDeque}};
+use std::time::{Instant, Duration};
 use utils::*;
 mod utils;
 mod day1;
@@ -9,12 +10,20 @@ fn main() {
     let day = 1;
     let part = 1;
 
-    let result = match (day, part) {
-        (1, 1) => day1::part1(&read_file("1")),
-        (1, 2) => day1::part2(&read_file("1")),
-        (_, _) => panic!(),
-    };
+    let contents = read_file(&day.to_string());
+    let start = Instant::now();
+    for i in 0..10000 {
+        let result = match (day, part) {
+            (1, 1) => day1::part1(&contents),
+            (1, 2) => day1::part2(&contents),
+            (_, _) => panic!(),
+        };
+        std::hint::black_box(result);
+        // println!("{result}");
+    }
+    let end = Instant::now();
+    let duration = end - start;
 
-    println!("{result}");
+    println!("took {duration:?}");
 }
 
